19e0a1737dda121744c972d1914fbf91c79b6458
ricardoBento <ricardo@flowhospitalitytraining.co.uk>
2020-10-01 11:58:42 +0100

    components and assets added, not tested run yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,138 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:vector_math/vector_math.dart' show radians, Vector3;
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'components/app_fab_circular_menu.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(WhiteLabelApp());
+}
 
-class MyApp extends StatelessWidget {
+class WhiteLabelApp extends StatelessWidget {
+  final GlobalKey<FabCircularMenuState> fabKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-
+    final primaryColor = Theme.of(context).primaryColor;
     return MaterialApp(
-        title: 'FlutterBase',
-        home: Scaffold(
-            body: SizedBox.expand(child: RadialMenu()) 
-        )
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+                color: Colors.amber,
+                image: DecorationImage(
+                    image: AssetImage("assets/images/wl-bg.png"),
+                    fit: BoxFit.cover)),
+            child: Center(
+              child: Text('Welcome to Flow White Label with Futter',
+                  style: TextStyle(color: Colors.amber)),
+            ),
+          ),
+          floatingActionButton: Builder(
+            builder: (context) => AppFabCircularMenu(
+              key: fabKey,
+              // Cannot be `Alignment.center`
+              alignment: Alignment.bottomRight,
+              ringColor: Colors.white.withAlpha(25),
+              ringDiameter: 500.0,
+              ringWidth: 150.0,
+              fabSize: 64.0,
+              fabElevation: 8.0,
+
+              // Also can use specific color based on wether
+              // the menu is open or not:
+              fabOpenColor: Colors.red,
+              fabCloseColor: Colors.blue,
+              // These properties take precedence over fabColor
+              fabColor: Colors.white,
+              fabOpenIcon: Icon(Icons.menu, color: primaryColor),
+              fabCloseIcon: Icon(Icons.close, color: primaryColor),
+              fabMargin: const EdgeInsets.all(16.0),
+              animationDuration: const Duration(milliseconds: 800),
+              animationCurve: Curves.easeInOutCirc,
+              onDisplayChange: (isOpen) {
+                // _showSnackBar(context, "The menu is ${isOpen ? "open" : "closed"}");
+              },
+              children: <Widget>[
+                RawMaterialButton(
+                  onPressed: () {
+                    // _showSnackBar(context, "You pressed 1");
+                  },
+                  shape: CircleBorder(),
+                  padding: const EdgeInsets.all(24.0),
+                  child: Icon(Icons.looks_one, color: Colors.white),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    // _showSnackBar(context, "You pressed 2");
+                  },
+                  shape: CircleBorder(),
+                  padding: const EdgeInsets.all(24.0),
+                  child: Icon(Icons.looks_two, color: Colors.white),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    // _showSnackBar(context, "You pressed 3");
+                  },
+                  shape: CircleBorder(),
+                  padding: const EdgeInsets.all(24.0),
+                  child: Icon(Icons.looks_3, color: Colors.white),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    // _showSnackBar(context, "You pressed 4. This one closes the menu on tap");
+                    fabKey.currentState.close();
+                  },
+                  shape: CircleBorder(),
+                  padding: const EdgeInsets.all(24.0),
+                  child: Icon(Icons.looks_4, color: Colors.white),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
 
-class RadialMenu extends StatefulWidget {
-  createState() => _RadialMenuState();
-}
-
-class _RadialMenuState extends State<RadialMenu> with SingleTickerProviderStateMixin {
-  
-  AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(duration: Duration(milliseconds: 900), vsync: this);
-                  // ..addListener(() => setState(() {}));
-  }
+class RadialMenuApp extends StatelessWidget {
+  final GlobalKey<FabCircularMenuState> fabKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return RadialAnimation(controller: controller);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-}
-
-
-class RadialAnimation extends StatelessWidget {
-    RadialAnimation({ Key key, this.controller }) :
-
-    translation = Tween<double>(
-      begin: 0.0,
-      end: 100.0,
-    ).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Curves.elasticOut
-      ),
-    ),
-
-    scale = Tween<double>(
-      begin: 1.5,
-      end: 0.0,
-    ).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Curves.fastOutSlowIn
-      ),
-    ),
-
-    rotation = Tween<double>(
-      begin: 0.0,
-      end: 360.0,
-    ).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Interval(
-          0.0, 0.7,
-          curve: Curves.decelerate,
+    final primaryColor = Theme.of(context).primaryColor;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          // color: const Color(0xFF192A56),
+          decoration: BoxDecoration(
+              color: Colors.amber,
+              image: DecorationImage(
+                  image: AssetImage("assets/images/wl-bg.png"),
+                  fit: BoxFit.cover)),
+          child: Center(
+            child: RaisedButton(
+              onPressed: () {
+                // The menu can be handled programatically using a key
+                if (fabKey.currentState.isOpen) {
+                  fabKey.currentState.close();
+                } else {
+                  fabKey.currentState.open();
+                }
+              },
+              color: Colors.white,
+              child: Text('Welcome to Flow White Label with Futter',
+                  style: TextStyle(color: Colors.amber)),
+            ),
+          ),
         ),
       ),
-    ),
-    
-  super(key: key);
-
-  final AnimationController controller;
-  final Animation<double> rotation;
-  final Animation<double> translation;
-  final Animation<double> scale;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (context, widget) { 
-        return Transform.rotate(
-          angle: radians(rotation.value),
-          child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            _buildButton(0, color: Colors.red, icon: FontAwesomeIcons.thumbtack),
-            _buildButton(45, color: Colors.green, icon:FontAwesomeIcons.sprayCan),
-            _buildButton(90, color: Colors.orange, icon: FontAwesomeIcons.fire),
-            _buildButton(135, color: Colors.blue, icon:FontAwesomeIcons.kiwiBird),
-            _buildButton(180, color: Colors.black, icon:FontAwesomeIcons.cat),
-            _buildButton(225, color: Colors.indigo, icon:FontAwesomeIcons.paw),
-            _buildButton(270, color: Colors.pink, icon: FontAwesomeIcons.bong),
-            _buildButton(315, color: Colors.yellow, icon:FontAwesomeIcons.bolt),
-            Transform.scale(
-              scale: scale.value - 1,
-              child: FloatingActionButton(child: Icon(FontAwesomeIcons.timesCircle), onPressed: _close, backgroundColor: Colors.red),
-            ),
-            Transform.scale(
-              scale: scale.value,
-              child: FloatingActionButton(child: Icon(FontAwesomeIcons.solidDotCircle), onPressed: _open),
-            )
-        
-        ])
-      );
-    });
-  }
-
-  _open() {
-    controller.forward();
-  }
-
-  _close() {
-    controller.reverse();
-  }
-
-  _buildButton(double angle, { Color color, IconData icon }) {
-    final double rad = radians(angle);
-    return Transform(
-      transform: Matrix4.identity()..translate(
-        (translation.value) * cos(rad), 
-        (translation.value) * sin(rad)
-      ),
-
-      child: FloatingActionButton(
-        child: Icon(icon), backgroundColor: color, onPressed: _close, elevation: 0)
     );
   }
+
+  // void _showSnackBar (BuildContext context, String message) {
+  //   Scaffold.of(context).showSnackBar(
+  //       SnackBar(
+  //         content: Text(message),
+  //         duration: const Duration(milliseconds: 1000),
+  //       )
+  //   );
+  // }
 }
